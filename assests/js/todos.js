73bbd6212439e3c1f9c9f4f67d528fa84d99eade
markmjm <markmnei@gmail.com>
2017-11-19 20:55:19 -0500

    initial commit

---
+++ assests/js/todos.js
@@ -0,0 +1,22 @@
+//Check off to do by clicking
+
+$('ul').on('click', 'li', function () {
+    $(this).toggleClass("completed");
+});
+
+$('ul').on('click', 'span', function (e) {
+    e.stopPropagation();
+    $(this).css('backgroundColor', "green")
+    $(this).parent().fadeOut(1000, function () {
+        $(this).remove();
+    })
+});
+
+$("input[type='text']").on('keypress', function (e) {
+    var newTodo = $(this).val();
+    if (e.which === 13) {
+        thetext = "<li><span class=\"delete\"><img src=\"assests/images/icons8-empty-trash-50.png\" width=\"20\"/></span> " + newTodo + "</li>";
+        $(this).val("");
+        $('ul').append(thetext);
+    }
+})
